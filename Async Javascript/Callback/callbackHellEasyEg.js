aaa355c7d74a8b5e97494503f41ec30dcc5c0026
Ig-Ronit <ronitkene7@gmail.com>
2025-08-28 15:23:09 +0530

    added Promise

--- Async Javascript/Callback/callbackHellEasyEg.js
+++ Async Javascript/Callback/callbackHellEasyEg.js
@@ -2,23 +2,23 @@ function apiCall1(apiCall2Callback) {
   console.log("We are ready to execute API call 1");
   setTimeout(() => {
     console.log("Api call 1 is done");
-    console.log("calling API call 2 now");
     apiCall2Callback();
-  }, 3000);
+  }, 5000);
 }
 
 function apiCall2(apiCall3Callback) {
+  console.log("calling API call 2 now");
   setTimeout(() => {
     console.log("Api call 2 is done");
-    console.log("calling API call 3 now");
     apiCall3Callback();
-  }, 3000);
+  }, 5000);
 }
 
 function apiCall3() {
+  console.log("calling API call 3 now");
   setTimeout(() => {
     console.log("Api call 3 is done");
-  }, 3000);
+  }, 5000);
 }
 
 apiCall1(() => {
