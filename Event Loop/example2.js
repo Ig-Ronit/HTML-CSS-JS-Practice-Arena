ae2e862ed1321bad9080caab293b42adf287b3e1
Ronit Kene <ronitkene7@gmail.com>
2025-09-30 09:27:50 +0530

    Event Loop

---
+++ Event Loop/example2.js
@@ -0,0 +1,37 @@
+console.log(1);
+
+console.log(2);
+
+setTimeout(() => {
+  console.log(3);
+}, 0);
+
+setTimeout(() => {
+  console.log(4);
+}, 100);
+
+setTimeout(() => {
+  console.log(5);
+}, 0);
+
+new Promise((resolve) => {
+  console.log(6);
+
+  resolve();
+});
+
+new Promise((resolve) => {
+  setTimeout(() => {
+    console.log(7);
+    resolve();
+  }, 0);
+});
+
+new Promise((resolve) => {
+  setTimeout(() => {
+    console.log(8);
+    resolve();
+  }, 100);
+});
+
+console.log(9);
